7c441664d11a558d39999ecb373701d9956ab740
Daniel Monyak <danmonyak@gmail.com>
2023-03-02 01:09:36 -0500

    Update haplotypeCaller.sh

--- haplotypeCaller.sh
+++ haplotypeCaller.sh
@@ -5,10 +5,10 @@ sampleName="SRR17843648_unmapped"
 gatk_path="/hpc/group/ryserlab/gatk-4.3.0.0/gatk"
 ref_fasta="/hpc/group/ryserlab/working/refdata-gex-GRCh38-2020-A/fasta/genome.fa"
 input_bam="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/cromwell-executions/RNAseq/2617d9f2-b516-4dc6-b4e7-990f2ccdc12e/call-ApplyBQSR/execution/SRR17843648_CR.aligned.duplicates_marked.recalibrated.bam"
-interval_list="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/cromwell-executions/RNAseq/2617d9f2-b516-4dc6-b4e7-990f2ccdc12e/call-ScatterIntervalList/execution/out/temp_0003_of_6/3scattered.interval_list"
-base_name=
-dbSNP_vcf=
-ref_dict=
+interval_list="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/cromwell-executions/RNAseq/2617d9f2-b516-4dc6-b4e7-990f2ccdc12e/call-ScatterIntervalList/execution/out/temp_0004_of_6/3scattered.interval_list"
+base_name=sampleName".hc"
+dbSNP_vcf="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/realdata/Homo_sapiens_assembly38.dbsnp138.vcf"
+ref_dict="/hpc/group/ryserlab/working/refdata-gex-GRCh38-2020-A/fasta/genome.dict"
 
 ${gatk_path} --java-options "-Xms6000m -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10" \
 		HaplotypeCaller \
