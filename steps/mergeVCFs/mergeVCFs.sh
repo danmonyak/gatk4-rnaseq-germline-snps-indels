7896aa8f28740136a2d5c0059ea4839eabb0c93b
Daniel Monyak <danmonyak@gmail.com>
2023-03-02 01:40:51 -0500

    Update mergeVCFs.sh

--- steps/mergeVCFs/mergeVCFs.sh
+++ steps/mergeVCFs/mergeVCFs.sh
@@ -14,13 +14,21 @@ sampleName="SRR17843648_unmapped"
 input_vcfs=$HaplotypeCallerOutputVcf
 output_vcf_name=$sampleName".g.vcf.gz"
 
-/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels
+gatkDir="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/cromwell-executions/RNAseq/2617d9f2-b516-4dc6-b4e7-990f2ccdc12e/call-HaplotypeCaller"
+manualDir="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/steps/haplotypeCaller"
+
+vcfFilename="SRR17843648_CR.hc.vcf.gz"
 
 gatk_path="/hpc/group/ryserlab/gatk-4.3.0.0/gatk"
 
 ${gatk_path} --java-options "-Xms2000m"  \
             MergeVcfs \
-            --INPUT ${sep=' --INPUT ' input_vcfs} \
+            --INPUT $gatkDir/shard-0/execution/$vcfFilename \
+            --INPUT $gatkDir/shard-1/execution/$vcfFilename \
+            --INPUT $gatkDir/shard-2/execution/$vcfFilename \
+            --INPUT $manualDir/$vcfFilename \
+            --INPUT $gatkDir/shard-4/execution/$vcfFilename \
+            --INPUT $gatkDir/shard-5/execution/$vcfFilename \
             --OUTPUT ${output_vcf_name}
 
 echo "Done!"
