d21f916063db5feb24e13caf2f405a43ee97eb66
Daniel Monyak <danmonyak@gmail.com>
2023-03-02 01:30:07 -0500

    Update haplotypeCaller.sh

--- steps/haplotypeCaller/haplotypeCaller.sh
+++ steps/haplotypeCaller/haplotypeCaller.sh
@@ -17,7 +17,7 @@ gatk_path="/hpc/group/ryserlab/gatk-4.3.0.0/gatk"
 ref_fasta="/hpc/group/ryserlab/working/refdata-gex-GRCh38-2020-A/fasta/genome.fa"
 input_bam="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/cromwell-executions/RNAseq/2617d9f2-b516-4dc6-b4e7-990f2ccdc12e/call-ApplyBQSR/execution/SRR17843648_CR.aligned.duplicates_marked.recalibrated.bam"
 interval_list="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/cromwell-executions/RNAseq/2617d9f2-b516-4dc6-b4e7-990f2ccdc12e/call-ScatterIntervalList/execution/out/temp_0004_of_6/4scattered.interval_list"
-base_name=sampleName".hc"
+base_name=$sampleName".hc"
 dbSNP_vcf="/hpc/group/ryserlab/working/gatk4/gatk4-rnaseq-germline-snps-indels/realdata/Homo_sapiens_assembly38.dbsnp138.vcf"
 ref_dict="/hpc/group/ryserlab/working/refdata-gex-GRCh38-2020-A/fasta/genome.dict"
 
